cb03837c5258d4c5f9604e1a2e4a25f77e72c3bc
FanaHOVA <fanahova@gmail.com>
2014-04-13 16:03:57 +0200

    Comments

--- randomizer.py
+++ randomizer.py
@@ -1,12 +1,16 @@
 import random
 
+# Ask the user for teams and the odds
+
 home = raw_input("Who's the home team? ")
 away = raw_input("Who are they playing against? ")
 odds = raw_input("What are the odds for the home team? (Only enter the number, no %) ")
 
-tokens = []
-homet = int(odds)
+tokens = [] #Empty list to be filled from loops
+homet = int(odds) 
 awayt = 100 - int(odds)
+
+#Add elements to the list for each team based on the odds
 
 for i in range(homet):
     tokens.append("a")
@@ -15,6 +19,7 @@ for i in range(awayt):
     tokens.append("b")
 
 result = random.choice(tokens)
+
 if result == "a":
     print home +" will win"
 elif result == "b":
